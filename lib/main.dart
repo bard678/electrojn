7ab4ae86538fd9f46704baefe99cb6dec5d988eb
bard678 <bard76217@gmail.com>
2024-03-28 20:34:10 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
+import 'package:untitled2/coloredresistors.dart';
 import 'package:untitled2/settings.dart';
 import 'package:untitled2/voltage.dart';
 import 'package:untitled2/voltage_divider.dart';
@@ -16,7 +17,7 @@ class ThemeGo extends ChangeNotifier{
   }
   void setTheme(ThemeData data){
     _data=data;
-   notifyListeners();
+    notifyListeners();
   }
 }
 ThemeData _data=ThemeData.light();
@@ -29,13 +30,14 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create:(context)=>ThemeGo() ,
       child: Consumer<ThemeGo>(
-       builder: (b,i,c)=> MaterialApp(
-         theme: _data,
-         debugShowCheckedModeBanner: false,
-         title: 'Flutter Demo',
+        builder: (b,i,c)=> MaterialApp(
+          color: Colors.black,
+          theme: _data,
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
 
-         home: const MyHomePage(title: 'Flutter Demo Home Page'),
-       ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
       ),
     );
   }
@@ -75,248 +77,287 @@ class _MyHomePageState extends State<MyHomePage> {
     "Simple audio amplifier",
     "Tl494 circuit"
   ];
+  List<String> mostCircuit=[
+    "assets/flasher.jpg",
+    "assets/flasher.jpg",
+    "assets/flasher.jpg",
+    "assets/flasher.jpg",
+    "assets/flasher.jpg",
+    "assets/flasher.jpg",
+
+  ];
   List<String> commonMistak=[
     "Power supply",
     "Audio circuit designing",
     "Battery charger circuit designing",
     "Switch mode inverter"
   ];
+  List<String> images=[
+    "assets/transistor.PNG",
+    "assets/resistor.PNG",
+    "assets/smdres.PNG",
+    "assets/img.png",
+    "assets/stepdown.PNG",
+    "assets/opamp.PNG",
+    "assets/voltage.PNG",
+
+
+  ];
   int tap=0;
   bool addIcon=false;
   @override
   Widget build(BuildContext context) {
 
-List<Widget> body=[
-  ListView.builder(
-shrinkWrap: true,
-    itemCount:calculations.length ,
-    itemBuilder: (c,i){
-      return Slidable(
-        startActionPane: ActionPane(
-            motion: const ScrollMotion(), children: [
-          SlidableAction(
-              icon: Icons.delete
-              ,onPressed: (c){setState(() {
-            g.removeAt(i);
-          });})
-        ]),
-        child: Column(
-          children: [
-            ListTile(
-              onTap: (){
-              if(i==6)
-              {
-                setState(() {
-                  Navigator.push(context, MaterialPageRoute(builder: (builder)=>const VoltageDivider(
-                  )));
-                });
-              }
-              if(i==2)
-              {
-                setState(() {
-                  Navigator.push(context, MaterialPageRoute(builder: (builder)=> VoltageDiv(
-                    data:_data
-                  )));
-                });
-              }
-              },
-              title:Text( calculations[i],style: const TextStyle(fontSize: 14),),
-            ),
-            const Divider(
-              height: 2,
-            )
-          ],
+    List<Widget> body=[
+      ListView.builder(
+          shrinkWrap: true,
+          itemCount:calculations.length ,
+          itemBuilder: (c,i){
+            return Slidable(
+              startActionPane: ActionPane(
+                  motion: const ScrollMotion(), children: [
+                SlidableAction(
+                    icon: Icons.delete
+                    ,onPressed: (c){setState(() {
+                  g.removeAt(i);
+                });})
+              ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                 leading:SizedBox(
+                   width: 40,
+                     height: 50,
+                     child: Image.asset(images[i],scale: 1,)),
+                    onTap: (){
+                      if(i==6)
+                      {
+                        setState(() {
+                          Navigator.push(context, MaterialPageRoute(builder: (builder)=>const VoltageDivider(
+                          )));
+                        });
+                      }
+                      if(i==2)
+                      {
+                        setState(() {
+                          Navigator.push(context, MaterialPageRoute(builder: (builder)=> VoltageDiv(
+                              data:_data
+                          )));
+                        });
+                      }
+                      if(i==1)
+                      {
+                        setState(() {
+                          Navigator.push(context, MaterialPageRoute(builder: (builder)=> ColoredResistors(
+
+                          )));
+                        });
+                      }
+                    },
+                    title:Text( calculations[i],style: const TextStyle(fontSize: 14),),
+                  ),
+                  const Divider(
+                    height: 2,
+                  )
+                ],
+              ),
+            );
+          }),
+      ListView.builder(
+          itemCount:mostCommon.length ,
+          itemBuilder: (c,i){
+            return Slidable(
+              startActionPane: ActionPane(
+                  motion: const ScrollMotion(), children: [
+                SlidableAction(
+                    icon: Icons.delete
+                    ,onPressed: (c){setState(() {
+                  g.removeAt(i);
+                });})
+              ]),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: SizedBox(
+                      width: 40,height: 50,child: Image.asset(mostCircuit[i]),
+                    ),
+                    title:Text( mostCommon[i],style: const TextStyle(fontSize: 14),),
+                  ),
+                  const Divider(
+                    height: 2,
+                  )
+                ],
+              ),
+            );
+          }),
+      ListView.builder(
+          itemCount:commonMistak.length ,
+          itemBuilder: (c,i){
+            return Slidable(
+              startActionPane: ActionPane(
+                  motion: const ScrollMotion(), children: [
+                SlidableAction(
+                    icon: Icons.delete
+                    ,onPressed: (c){setState(() {
+                  g.removeAt(i);
+                });})
+              ]),
+              child: Column(
+                children: [
+                  ListTile(
+                    title:Text( commonMistak[i],style: const TextStyle(fontSize: 14),),
+                  ),
+                  const Divider(height: 2,)
+                ],
+              ),
+            );
+          }),
+      ListView.builder(
+          itemCount:g.length ,
+          itemBuilder: (c,i){
+            return Slidable(
+              startActionPane: ActionPane(
+                  motion: const ScrollMotion(), children: [
+                SlidableAction(
+                    icon: Icons.delete
+                    ,onPressed: (c){setState(() {
+                  g.removeAt(i);
+                });})
+              ]),
+              child: const ListTile(
+                title:Text( "hi1"),
+              ),
+            );
+          }),
+      ListView.builder(
+          itemCount:g.length ,
+          itemBuilder: (c,i){
+            return Slidable(
+              startActionPane: ActionPane(
+                  motion: const ScrollMotion(), children: [
+                SlidableAction(
+                    icon: Icons.delete
+                    ,onPressed: (c){setState(() {
+                  g.removeAt(i);
+                });})
+              ]),
+              child: const ListTile(
+                title:Text( "hi1"),
+              ),
+            );
+          }),
+    ] ;
+    return WillPopScope(
+      onWillPop: ()async{
+        return await showDialog(context: context, builder: (builder)=>StatefulBuilder(
+          builder: (BuildContext context,set) {
+            return  AlertDialog(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero
+              ),
+              title: const Text("Are you sure you want to exit?",style: TextStyle(color: Colors.grey,fontSize: 18),),
+              content: Row(children: [
+                Checkbox(
+                    activeColor: Colors.green,
+
+                    value: ask, onChanged: (v){
+                  setState(() {
+                  });
+                  set(() {
+                    ask=v!;
+                  });
+                }),const Text("Don't ask me again"),
+              ],),
+              actions: [
+                TextButton(onPressed: (){}, child:  const Text("No",style: TextStyle(color: Colors.green),)),
+                TextButton(onPressed: (){
+
+                }, child: const Text("Yes",style: TextStyle(color: Colors.green),))
+              ],
+            ); },
         ),
-      );
-    }),
-  ListView.builder(
-    itemCount:mostCommon.length ,
-    itemBuilder: (c,i){
-      return Slidable(
-        startActionPane: ActionPane(
-            motion: const ScrollMotion(), children: [
-          SlidableAction(
-              icon: Icons.delete
-              ,onPressed: (c){setState(() {
-            g.removeAt(i);
-          });})
-        ]),
-        child: Column(
-          children: [
-            ListTile(
-              title:Text( mostCommon[i],style: const TextStyle(fontSize: 14),),
-            ),
-            const Divider(
-              height: 2,
-            )
-          ],
-        ),
-      );
-    }),
-  ListView.builder(
-    itemCount:commonMistak.length ,
-    itemBuilder: (c,i){
-      return Slidable(
-        startActionPane: ActionPane(
-            motion: const ScrollMotion(), children: [
-          SlidableAction(
-              icon: Icons.delete
-              ,onPressed: (c){setState(() {
-            g.removeAt(i);
-          });})
-        ]),
-        child: Column(
-          children: [
-            ListTile(
-              title:Text( commonMistak[i],style: const TextStyle(fontSize: 14),),
-            ),
-            const Divider(height: 2,)
-          ],
-        ),
-      );
-    }),
-  ListView.builder(
-      itemCount:g.length ,
-      itemBuilder: (c,i){
-        return Slidable(
-          startActionPane: ActionPane(
-              motion: const ScrollMotion(), children: [
-            SlidableAction(
-                icon: Icons.delete
-                ,onPressed: (c){setState(() {
-              g.removeAt(i);
-            });})
-          ]),
-          child: const ListTile(
-            title:Text( "hi1"),
-          ),
+
         );
-      }),
-  ListView.builder(
-      itemCount:g.length ,
-      itemBuilder: (c,i){
-        return Slidable(
-          startActionPane: ActionPane(
-              motion: const ScrollMotion(), children: [
-            SlidableAction(
-                icon: Icons.delete
-                ,onPressed: (c){setState(() {
-              g.removeAt(i);
-            });})
-          ]),
-          child: const ListTile(
-            title:Text( "hi1"),
-          ),
-        );
-      }),
-] ;
-return WillPopScope(
-  onWillPop: ()async{
-    return await showDialog(context: context, builder: (builder)=>StatefulBuilder(
-      builder: (BuildContext context,set) {
-        return  AlertDialog(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.zero
-          ),
-          title: const Text("Are you sure you want to exit?",style: TextStyle(color: Colors.grey,fontSize: 18),),
-          content: Row(children: [
-            Checkbox(
-                activeColor: Colors.green,
-
-                value: ask, onChanged: (v){
-              setState(() {
-              });
-              set(() {
-                ask=v!;
-              });
-            }),const Text("Don't ask me again"),
-          ],),
-          actions: [
-            TextButton(onPressed: (){}, child:  Text("No",style: TextStyle(color: Colors.green),)),
-            TextButton(onPressed: (){
-
-            }, child: const Text("Yes",style: TextStyle(color: Colors.green),))
-          ],
-        ); },
-    ),
-
-    );
-  },
-  child: DefaultTabController(
+      },
+      child: DefaultTabController(
         length: 5,
         child: Scaffold(
-          appBar: AppBar(
-  leading:  PopupMenuButton<String>(
-    onSelected: (v){
-      if(v=="s"){
-        Navigator.push(context, MaterialPageRoute(builder: (context){return const SettingsScreen();}));
-      }
-      print(v);
-    },
-    child: const Icon(Icons.more_vert),
-      itemBuilder: (itemBuilder)=>
-  [  const PopupMenuItem(
-    value: "s",
-      child: Text("Settings"),
-    ),
-  // PopupMenuItem(
-  //   value: "s",
-  //     child: Text("Settings"),
-  //   ),
-  const PopupMenuItem(
-    value: "sh",
-      child: Text("Share"),
-    ),
-  ]
-  ),
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              leading:  PopupMenuButton<String>(
+                  onSelected: (v){
+                    if(v=="s"){
+                      Navigator.push(context, MaterialPageRoute(builder: (context){return const SettingsScreen();}));
+                    }
+                    print(v);
+                  },
+                  child: const Icon(Icons.more_vert,color: Colors.black87,),
+                  itemBuilder: (itemBuilder)=>
+                  [  const PopupMenuItem(
+                    value: "s",
+                    child: Text("Settings"),
+                  ),
+                    // PopupMenuItem(
+                    //   value: "s",
+                    //     child: Text("Settings"),
+                    //   ),
+                    const PopupMenuItem(
+                      value: "sh",
+                      child: Text("Share"),
+                    ),
+                  ]
+              ),
 
-            title: const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text("ElectroJN",textAlign: TextAlign.left,),
-              ],
-            ),
-            bottom: TabBar(
-              isScrollable: true,
-              indicatorColor:Colors.green ,
-              dividerColor: Colors.green,
-              labelColor: Colors.green,
-              onTap: (i){
+              title: const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("ElectroJN",textAlign: TextAlign.left,style: TextStyle(color: Colors.black87),),
+                ],
+              ),
+              bottom: TabBar(
+                unselectedLabelColor: Color.lerp(Colors.grey, Colors.black, 0.7),
+                  isScrollable: true,
+                  indicatorColor:Colors.green ,
+                  dividerColor: Colors.green,
+                  labelColor: Colors.black87,
 
-                setState(() {
-                  tap=i;
+                  onTap: (i){
 
-                  if(tap==4){setState(() {
-                    addIcon=true;
-                  });}else {
                     setState(() {
-                    addIcon=false;
-                  });
-                  }
-                  print(i);
-                  print(calculations.length);
-                });
-              },
-                tabs: const [
-               Tab(text: "Calculations",),
-         Tab(text: "Most common circuits",),
-         Tab(text: "Most common mistakes",),
-         Tab(text: "Tricks",),
-                  Tab(text: "Notes",),
-      ]
+                      tap=i;
+
+                      if(tap==4){setState(() {
+                        addIcon=true;
+                      });}else {
+                        setState(() {
+                          addIcon=false;
+                        });
+                      }
+                      print(i);
+                      print(calculations.length);
+                    });
+                  },
+                  tabs: const [
+                    Tab(text: "Calculations",),
+                    Tab(text: "Most common circuits",),
+                    Tab(text: "Most common mistakes",),
+                    Tab(text: "Tricks",),
+                    Tab(text: "Notes",),
+                  ]
 
 
 
+              ),
+              actions: [
+                addIcon==true?IconButton(onPressed: (){setState(() {
+                  g.add("a$_counter");
+                  ++_counter;
+                });}, icon: const Icon(Icons.add,color: Colors.black87,)):const SizedBox()],
             ),
-            actions: [
-              addIcon==true?IconButton(onPressed: (){setState(() {
-              g.add("a$_counter");
-              ++_counter;
-            });}, icon: const Icon(Icons.add)):const SizedBox()],
-          ),
-        body:body[tap]
+            body:body[tap]
         ),
       ),
-);
+    );
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -11,107 +11,109 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  
+
   List<String> title=["Confirmation on exit","Change language","Change theme"];
   List<String> supTitle=["Asking before exiting","Choose your own language","customise theme"];
-List<IconData> icons=[Icons.exit_to_app,Icons.language,Icons.room_preferences_outlined];
+  List<IconData> icons=[Icons.exit_to_app,Icons.language,Icons.room_preferences_outlined];
   int radio=0;
   bool checked=false;
   bool ask=false;
   @override
   Widget build(BuildContext context) {
-    
+
     return  Scaffold(
       appBar: AppBar(
-        title: const Text("Settings"),
+        title: Text("Settings"),
         // backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child:
-
-           Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child:
+        
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Padding(
+              Padding(
                 padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                 child: Text("Application preferences",style: TextStyle(color: Colors.green),),
               ),
-              SizedBox(
+              Container(
+              
                 height: MediaQuery.of(context).size.height-AppBar().preferredSize.height-40,
                 child: ListView.builder(itemCount: title.length,itemBuilder: (context,i){
-
+              
                   return Column(
                     children: [
                       ListTile(
                         trailing:i==0? Checkbox(
-                          activeColor: Colors.green,
+                            activeColor: Colors.green,
                             value: checked, onChanged: (v){setState(() {
                           checked=v!;
-                            });}):const SizedBox(),
+                        });}):SizedBox(),
                         onTap: (){
-                        if(i==2){
-                          showDialog(context: context, builder: (builder)=>StatefulBuilder(
-                            builder: (c,set){return
-                              AlertDialog(
-                                shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.zero
-                                ),
-                                title: const Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text("Choose theme"),
-                                  ],
-                                ),content: SizedBox(
-                                height: 100,
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-
-                                        Radio(
-                                          activeColor: Colors.green,
-                                          value: 0, groupValue: radio, onChanged: (v){
-                                          set(() {
-                                            Provider.of<ThemeGo>(context,listen: false).setTheme(ThemeData.light());
+                          if(i==2){
+                            showDialog(context: context, builder: (builder)=>StatefulBuilder(
+                              builder: (c,set){return
+                                AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.zero
+                                  ),
+                                  title: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text("Choose theme"),
+                                    ],
+                                  ),content: SizedBox(
+                                  height: 100,
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+              
+                                          Radio(
+                                            activeColor: Colors.green,
+                                            value: 0, groupValue: radio, onChanged: (v){
+                                            set(() {
+                                              Provider.of<ThemeGo>(context,listen: false).setTheme(ThemeData.light());
+                                              radio=v as int;
+                                            });
+                                          }
+                                            ,
+                                          ),
+                                          SizedBox(width: 20,),  Text('Light'),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+              
+                                          Radio(
+                                            focusColor: Colors.green,
+                                            activeColor: Colors.green,
+                                            value: 1, groupValue: radio, onChanged: (v){set(() {
+                                            Provider.of<ThemeGo>(c,listen: false).setTheme(ThemeData.dark());
                                             radio=v as int;
-                                          });
-                                        }
-                                          ,
-                                        ),
-                                        const SizedBox(width: 20,),  const Text('Light'),
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-
-                                        Radio(
-                                          focusColor: Colors.green,
-                                          activeColor: Colors.green,
-                                          value: 1, groupValue: radio, onChanged: (v){set(() {
-                                          Provider.of<ThemeGo>(c,listen: false).setTheme(ThemeData.dark());
-                                          radio=v as int;
-                                        } );}
-                                          ,
-                                        ) ,
-                                        const SizedBox(width: 20,),
-                                        const Text('Dark'),
-
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),);},
-
-                          ));
-                        }
-
+                                          } );}
+                                            ,
+                                          ) ,
+                                          SizedBox(width: 20,),
+                                          Text('Dark'),
+              
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),);},
+              
+                            ));
+                          }
+              
                         },
-                      leading: Icon(icons[i]),
+                        leading: Icon(icons[i]),
                         title: Text(title[i]),subtitle: Text(supTitle[i]),),
-                      const Divider(
+                      Divider(
                         height: 2,
                       )
                     ],
@@ -119,7 +121,8 @@ List<IconData> icons=[Icons.exit_to_app,Icons.language,Icons.room_preferences_ou
                 }),
               )
             ],
-
+        
+          ),
         ),
       ),
     );
